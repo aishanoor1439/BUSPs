b1a2cd654ee6c2a05014e4505f02ad77177ed9ef
kanwaladreeskhan <khanadreiskhan@gmail.com>
2024-12-26 02:18:06 -0800

    unknown commit

--- BUSPs/Forms/UserDashboardForm.Designer.cs
+++ BUSPs/Forms/UserDashboardForm.Designer.cs
@@ -79,7 +79,7 @@
             this.label2.Size = new System.Drawing.Size(165, 25);
             this.label2.TabIndex = 3;
             this.label2.Text = "Active Societies";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Click += new System.EventHandler(this.label1_click);
             // 
             // groupBox1
             // 
